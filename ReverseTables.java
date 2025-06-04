eefb37e0adec72c35bf5fdb4033839a20c6ca05c
NithishKumarKamaraj <nithish.kumar.kamaraj.k@gmail.com>
2025-06-04 23:10:32 +0530

    Add files via upload

---
+++ ReverseTables.java
@@ -0,0 +1,20 @@
+// Write a program to print reverse tables
+
+import java.util.Scanner;
+
+public class ReverseTables {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter Starting number");
+    int sn = sc.nextInt();
+    System.out.println("Enter Ending number");
+    int en = sc.nextInt();
+    System.out.println("Enter the tables number");
+    int tn = sc.nextInt();
+    while(sn>=en){
+        System.out.println(sn+"*"+tn+"="+sn*tn);
+        sn--;
+        }   
+    }
+}
