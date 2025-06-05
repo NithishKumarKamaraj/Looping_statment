906ed89f3fa3ecf27aefb0ab52d9cc0c394f6bf7
NithishKumarKamaraj <nithish.kumar.kamaraj.k@gmail.com>
2025-06-05 22:13:39 +0530

    Add files via upload

---
+++ ASCII.java
@@ -0,0 +1,11 @@
+// Write a program to print the ASCII values
+
+
+public class ASCII {
+
+    public static void main(String[] args) {
+    for(int n=1;n<=255;n++){
+    System.out.println((char)n);
+    }
+}
+}
